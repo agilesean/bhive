95c6b5ad214367d6112364fb88e9d1b14298cb9b
agilesean <heuersster@gmail.com>
2013-09-05 13:23:51 -0400

    Initial Commit2

---
+++ bhive/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bhive::Application.config.secret_key_base = '8e8b47246516e8965a641eb4bbb78471d567b087d24051af7df9cd179c005b17715f72f52e23fbf9e6e86fb2d0695aba0d649d30787ce6cfa4924c60e23c75a1'
